90041f820067c9448cdd410943f660eb8e07d2e5
Sarah-Burnett <burnie8657@gmail.com>
2020-11-06 12:01:14 +0000

    inital commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import "../node_modules/jquery/dist/jquery.slim";
+import "../node_modules/@popperjs/core/dist/cjs/popper-lite";
+import "../node_modules/bootstrap/js/*.js";
+
+import { hello } from "./hello";
